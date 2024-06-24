68a77b8e9d564b00d5b7bf103771d6b25f971673
Amani Magdi Farg <amanim.farag@gmail.com>
2024-06-24 14:09:04 +0300

    initializing project's main changes

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Auther(models.Model):
+    name= models.CharField(max_lengh=100) 
+    def __str__(self):
+        return str(self.name)
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    auther = models.ForeignKey(Auther, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.title)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post, Auther
 
 # Create your views here.
+def main_view(request):
+    data = []
+    return render(request, "main.html", {"data":data})
