f2419e79df774e8b6f903a9cdaf0a293b31a5be0
Amani Magdi Farg <amanim.farag@gmail.com>
2024-06-24 15:35:05 +0300

    configure django debug toolbar

--- optimize_django_queries/settings.py
+++ optimize_django_queries/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #tools
+    "debug_toolbar",
     #project apps
     'posts'
 ]
@@ -49,6 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = 'optimize_django_queries.urls'
--- optimize_django_queries/urls.py
+++ optimize_django_queries/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from posts import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_view, name='main_view'),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
